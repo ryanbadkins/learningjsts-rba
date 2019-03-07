19cba510eb5cdce9979e89f3c2058ae0906310fb
ryan badkins <rbadawg9@gmail.com>
2019-03-07 15:30:42 -0700

    simple game clicking squares

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
 import './styles.css';
-console.log('Ready to Party in JS land with typescript');
+import { getRandomInt } from "./rng";
+
+const secretNumber = getRandomInt(1, 6);
+const squares = document.querySelectorAll('.square');
+
+
+squares.forEach((s, i) => {
+    if (i + 1 === secretNumber) {
+        (<HTMLDivElement>s).dataset.secret = 'true';
+    }
+})
+squares.forEach(s => s.addEventListener('click', handleClick));
+
+function handleClick() {
+    const div = this as HTMLDivElement;
+    if (div.dataset.secret) {
+        div.classList.add('winner');
+        squares.forEach(s => s.removeEventListener('click', handleClick));
+    } else {
+        div.classList.add('loser');
+    }
+}
+
