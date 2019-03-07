3441b94c61e372565b1b6798db71688b52b13eb4
ryan badkins <rbadawg9@gmail.com>
2019-03-07 14:28:01 -0700

    hard function stuff, reduce ect

--- spec/02_functions_spec.ts
+++ spec/02_functions_spec.ts
@@ -51,10 +51,124 @@ describe('functions', () => {
     describe('higher order functions', () => {
         /*takes one or more functons as arguments, or returns a function as it's result*/
         it('takes a function as an argument', () => {
+            const answer = formatters.formatName('Han', 'Solo');
+            expect(answer).toBe('Solo, Han');
+            expect(formatters.PI).toBe(3.1415);
 
-            const answer = formatters.formatName('ryan', 'adkins');
-            expect(answer).toBe('adkins, ryan');
-            expect(formatters.PI).toBe(3.141569);
+            expect(formatters.formatName('Han', 'Solo', (x) => x.toUpperCase())).toBe('SOLO, HAN');
+            const wrapInStars = wrap('***');
+            expect(wrapInStars('Tacos')).toBe('***Tacos***');
+            expect(formatters.formatName('Han', 'Solo', wrapInStars)).toBe('***Solo, Han***');
+
+            //const wrapInCarots: formatters.Transform = (x) => `^^^${x}^^^`;
+            const wrapInCarots = wrap('^^^');
+            expect(formatters.formatName('Han', 'Solo', wrapInCarots)).toBe('^^^Solo, Han^^^');
+
+            // function wrapInStars(what: string): string {
+            //     return `***${what}***`;
+            // }
+
+            function wrap(chars: string): formatters.Transform {
+                //closure
+                return (x) => `${chars}${x}${chars}`;
+            }
+        });
+    });
+    describe('array methods', () => {
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        it('taking a look at every member of an array', () => {
+            numbers.forEach((x) => console.log(x));
+        });
+        describe('methods that return new arrays', () => {
+            it('has a filter', () => {
+                function isEven(n: number): boolean {
+                    return n % 2 === 0;
+                }
+                const evens = numbers.filter(isEven)
+                expect(evens).toEqual([2, 4, 6, 8]);
+                expect(numbers).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            });
+            it('map', () => {
+
+                const doubled = numbers.map(n => n * 2);
+                expect(doubled).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18]);
+            });
+
+            it('do a practice', () => {
+                interface Vehicle {
+                    vin: string;
+                    makeAndModel: string;
+                    mileage: number;
+                }
+                const vehicles: Vehicle[] = [
+                    { vin: '9999', makeAndModel: 'Chevy Tahoe', mileage: 182000 },
+                    { vin: 'aka92', makeAndModel: 'Toyota Prius', mileage: 89999 },
+                    { vin: 'kduwi', makeAndModel: 'Ford Explorer', mileage: 99998 }
+                ];
+                //Give jeff the makeAndModel of each vehicle with < 100,000 miles on it
+                const lowMileageVehicles = vehicles
+                    .filter(v => v.mileage < 100000)
+                    .map(v => v.makeAndModel)
+                    ;
+
+                expect(lowMileageVehicles).toEqual(['Toyota Prius', 'Ford Explorer']);
+            });
+            describe('methods that produce a single (scalar) value', () => {
+                it('has methods that check the mebership of an array', () => {
+                    expect(numbers.some(n => n > 8)).toBe(true);
+                    expect(numbers.every(n => n % 2 === 0)).toBe(false);
+                    // function isEven(n: number): boolean {
+                    //     return n % 2 === 0;
+                    // }
+                    // expect(numbers.every(isEven)).toBe(false); 
+                });
+                it('has reduce', () => {
+                    expect(numbers.reduce((p, n) => p + n)).toBe(45);
+                    expect(numbers.reduce((p, n) => p + n, 100)).toBe(145);
+                });
+            });
+        });
+    });
+    describe('a demo', () => {
+        it('using reduce for something "real"', () => {
+            interface Vehicle {
+                vin: string;
+                makeAndModel: string;
+                mileage: number;
+            }
+            const vehicles: Vehicle[] = [
+                { vin: '9999', makeAndModel: 'Chevy Tahoe', mileage: 182000 },
+                { vin: 'aka92', makeAndModel: 'Toyota Prius', mileage: 89999 },
+                { vin: 'kduwi', makeAndModel: 'Ford Explorer', mileage: 99998 }
+            ];
+
+            interface HighestMileageVehicle {
+                vin: string;
+                mileage: number;
+            }
+
+            const seed: HighestMileageVehicle = {
+                vin: null,
+                mileage: -1
+            };
+
+            const answer = vehicles.reduce((p, n) => {
+                if (n.mileage > p.mileage) {
+                    return {
+                        vin: n.vin,
+                        mileage: n.mileage
+                    };
+                } else {
+                    return p
+                }
+
+            }, seed);
+
+            expect(answer).toEqual({
+                vin: '9999',
+                mileage: 182000
+            });
         });
     });
 });
--- src/formatters.ts
+++ src/formatters.ts
@@ -1,4 +1,7 @@
-export const formatName = function (first: string, last: string): string {
-    return `${last}, ${first}`;
+export type Transform = (x: string) => string;
+export const formatName = function (first: string, last: string, transform?: Transform): string {
+    const response = `${last}, ${first}`;
+    return transform ? transform(response) : response;
 }
-export const PI = 3.141569;
+
+export const PI = 3.1415; 
